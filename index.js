5df3a60f8e799300ecd1b37f4b3fa5d0fd9fbd80
MyoHtet <myohtet.cu2014@gmail.com>
2023-10-05 19:01:34 +0630

    add html

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 let express = require("express");
+
+let app = express();
+
 // server setup
-let server = express().listen(3000, () => {
+let server = app.listen(3000, () => {
   console.log("application is running on port 3000");
+});
+
+//route setup
+app.get("/", (res, req) => {
+  req.sendFile(__dirname + "/public/index.html");
 });
