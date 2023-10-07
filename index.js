a86f460bafeac6be9c4f0d3f0a09a36035c438a0
MyoHtet <myohtet.cu2014@gmail.com>
2023-10-07 07:20:26 +0630

    create socket server on server and client

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ let express = require("express");
 
 let app = express();
 
+//socket
+let socket = require("socket.io");
 // server setup
-let server = app.listen(3000, () => {
-  console.log("application is running on port 3000");
+let server = app.listen(4000, () => {
+  console.log("application is running on port 4000");
 });
 
 //route setup
 app.get("/", (res, req) => {
   req.sendFile(__dirname + "/index.html");
+});
+
+//socket setup
+let io = socket(server);
+io.on("connection", (socket) => {
+  console.log("User is connected" + socket.id);
 });
